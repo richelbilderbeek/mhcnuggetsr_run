0d81ccdd15baef822e57db6bf0ed8ee5e4da0b36
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-26 10:52:59 +0200

    Go Travis!

---
+++ scripts/run.R
@@ -0,0 +1,20 @@
+library(mhcnuggets)
+library(testthat)
+library(knitr)
+
+expect_true(is_mhcnuggets_installed())
+
+peptides_path <- get_example_filename("test_peptides.peps")
+expect_true(file.exists(peptides_path))
+
+mhc_1_haplotype <- "HLA-A02:01"
+expect_true(mhc_1_haplotype %in% get_trained_mhc_1_haplotypes())
+
+df <- epitope_predict(
+  mhc_class = "I",
+  peptides_path = peptides_path,
+  mhc = mhc_1_haplotype
+)
+
+kable(df)
+
